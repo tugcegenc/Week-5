ffd880270543f900b016fcd1a1cfdc20c7a3041f
tugcegenc <tugceegencc@gmail.com>
2024-01-04 01:36:18 +0300

    Try-catch pratik

---
+++ TryCatch/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter index: ");
+
+        int[] arr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        try {
+            int number = scanner.nextInt();
+
+            if (number < 0 || number > arr.length) {
+                throw new ArrayIndexOutOfBoundsException();
+            } else {
+                System.out.println("Element at the specified index: " + arr[number]);
+            }
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace(System.out);
+            System.out.print("Error: Index provided is outside the bounds of the array. Array size: " + arr.length + " ");
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+        }
+    }
+}
