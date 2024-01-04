b4975215a087973885172772be4e17f98362119e
tugcegenc <tugceegencc@gmail.com>
2024-01-04 10:44:27 +0300

    Try-catch pratik

--- TryCatch/src/Main.java
+++ TryCatch/src/Main.java
@@ -3,24 +3,31 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter index: ");
-
         int[] arr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-        try {
-            int number = scanner.nextInt();
+        System.out.print("Enter index: ");
+        int number = scanner.nextInt();
 
-            if (number < 0 || number > arr.length) {
+        try {
+            // Printing the element at the specified index by calling the method
+            printElementAtIndex(arr, number);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace(System.out);
+            System.out.print("Error: Index provided is outside the bounds of the array. Array size: " + arr.length);
+        }
+    }
+
+    // Method to print the element at a specified index
+    public static void printElementAtIndex(int[] arr, int index) {
+        try {
+            // Index validation and printing the element
+            if (index < 0 || index >= arr.length) {
                 throw new ArrayIndexOutOfBoundsException();
             } else {
-                System.out.println("Element at the specified index: " + arr[number]);
+                System.out.println("Element at the specified index: " + arr[index]);
             }
-
         } catch (ArrayIndexOutOfBoundsException e) {
-            e.printStackTrace(System.out); // Prints the error to the console
-            System.out.print("Error: Index provided is outside the bounds of the array. Array size: " + arr.length);
-        } catch (Exception e) {
-            e.printStackTrace(System.out); // Prints any other exceptions to the console
+            throw e; // Re-throwing the exception
         }
     }
 }
