6e66a458a1e1f2e6e7579230f6423e3518374609
tugcegenc <tugceegencc@gmail.com>
2024-01-10 23:12:07 +0300

    SQL

--- SQL/Odev3.sql
+++
@@ -1,4 +0,0 @@
-SELECT * FROM country WHERE country LIKE 'A%a';
-SELECT * FROM country WHERE LENGTH(country) >= 6 AND RIGHT(country, 1) = 'n';
-SELECT * FROM film WHERE LOWER(title) LIKE '%t%' OR UPPER(title) LIKE '%T%';
-SELECT * FROM film WHERE title LIKE 'C%' AND 'length' > 90 AND rental_rate = 2.99;
