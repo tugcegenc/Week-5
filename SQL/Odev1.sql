a6724f1537f18437573ce5a74f83d62ed3d281ca
tugcegenc <tugceegencc@gmail.com>
2024-01-10 22:28:06 +0300

    SQL

--- SQL/Odev1.sql
+++ SQL/Odev1.sql
@@ -1,4 +1,4 @@
-SELECT title, DESCRIPTION FROM film;
+SELECT title, description FROM film;
 SELECT * FROM film WHERE ( length > 60 AND length < 75 );
 SELECT * FROM film WHERE ( rental_rate = 0.99 AND replacement_cost = 12.99 ) OR ( replacement_cost = 28.99 );
 SELECT last_name FROM customer WHERE first_name = 'Mary';
